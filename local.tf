91c10d15a2a34b959d445dbe71e0a4d2431b3476
Chathushka <udanasanchitha4@gmail.com>
2024-12-11 18:53:16 +0530

    list access

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 resource "local_file" "pet"{
     filename = var.filename
-    content = var.content
+    content = "my pet is ${var.prefix[0]} Kitty"
     file_permission = var.file_permission
 }
 
